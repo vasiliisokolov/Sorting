907bce0312c60edb88c3ed70709ae5457d2a0caf
VasiliiSokolov <vasilysokolov@mail.ru>
2022-08-24 20:20:42 +0300

    first stage

--- Sorting/Sorting.cpp
+++ Sorting/Sorting.cpp
@@ -1,39 +1,35 @@
 ﻿#include <iostream>
 
-int find_smallest(float&);
 
 int main()
 {
     float first[15];
-    float second[15];
-    float temp;
+    
     std::cout << "Please, enter 15 numbers:" << std::endl;
     
     for (int i = 0; i < 15; i++)
     {
         std::cin >> first[i];
-        std::cout << first[i];
     }
-    for(int i = 0; i < 15; i++)
+	int temp;
+    for(int i = 0; i < 14; i++)
     {
-
+		for (int j = 0; j < 15 - i - 1; j++)
+		{
+			if (first[j] < first[j - 1])
+			{
+				temp = first[j];
+				first[j] = first[j + 1];
+				first[j + 1] = temp;
+			}
+			
+		}
 
     }
-}
-
-int find_smallest(float& first[15])
-{
-    int smallest = first[0];
-    int smallestIndex = 0;
-    for (int i = 1; i < 15; i++)
-    {
-        if (first[i] < smallest)
-        {
-            smallest = first[i];
-            smallestIndex = i;
-        }
+    for (int i = 0; i < 15; i++) {
+        std::cout << first[i] << " ";
     }
-    return smallestIndex;
+    
 }
 
 
